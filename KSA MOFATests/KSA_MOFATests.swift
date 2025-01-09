f53bed8831018bceb5a2998205cc276105930ff2
Brett & Kimber Kettler <brettkettler23@gmail.com>
2025-01-08 21:03:36 -0500

    Initial Commit

---
+++ KSA MOFATests/KSA_MOFATests.swift
@@ -0,0 +1,17 @@
+//
+//  KSA_MOFATests.swift
+//  KSA MOFATests
+//
+//  Created by Brett Kettler on 1/8/25.
+//
+
+import Testing
+@testable import KSA_MOFA
+
+struct KSA_MOFATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
